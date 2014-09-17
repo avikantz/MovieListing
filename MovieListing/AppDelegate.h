0a52f609791c15e2aa4594049c55c8c869351226
Avikant Saini <Avikant@Avikants-MacBook-Air.local>
2014-09-17 14:54:02 +0530

    Initial Commit

---
+++ MovieListing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieListing
+//
+//  Created by Avikant Saini on 9/17/14.
+//  Copyright (c) 2014 avikantz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MovieListing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MovieListing
+//
+//  Created by Avikant Saini on 9/17/14.
+//  Copyright (c) 2014 avikantz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
